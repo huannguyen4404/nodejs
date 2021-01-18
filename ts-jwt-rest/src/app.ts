a3cded547153b517bc7fa2e026b4dfb5e366d701
Henry Nguyen <thehuan@revu.net>
2021-01-18 10:05:29 +0700

    init Express

--- ts-jwt-rest/src/app.ts
+++ ts-jwt-rest/src/app.ts
@@ -1,8 +1,41 @@
-console.log('Express, jwt, typescript, mongoose')
+import compression from 'compression'
+import cors from 'cors'
+import express from 'express'
+import helmet from 'helmet'
 
-const abc = {
-  ab: 11,
-  bc: 12,
+class App {
+  public app: express.Application
+
+  constructor() {
+    this.app = express()
+    this.config()
+    this.routes()
+    this.dbConnect()
+  }
+
+  public config(): void {
+    this.app.set('port', process.env.PORT || 3000)
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+    this.app.use(compression())
+    this.app.use(helmet())
+    this.app.use(cors())
+  }
+
+  public routes(): void {
+    // Load routes
+  }
+
+  private dbConnect() {
+    // Connect with DB
+  }
+
+  public start(): void {
+    this.app.listen(this.app.get('port'), () => {
+      console.log(`API is running on ${this.app.get('port')}`)
+    })
+  }
 }
 
-console.log(abc)
+const app = new App()
+app.start()
