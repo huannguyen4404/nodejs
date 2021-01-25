34867f42f46c9b896a417dfa2016e4166452b77c
Henry Nguyen <huannguyen4404@gmail.com>
2021-01-25 15:20:24 +0700

    setup error handling

--- js-rest/src/server.js
+++ js-rest/src/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import * as dotenv from 'dotenv'
 import express from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
+import errorMiddleware from './common/middleware/error.middleware'
+import notFoundMiddleware from './common/middleware/notFound.middleware'
 import orderRouter from './orders/orders.routes'
 import productRouter from './products/products.routes'
 
@@ -16,10 +19,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(compression())
 app.use(helmet())
 app.use(cors())
+app.use(morgan('dev'))
 
 // routers
 app.use('/api/orders', orderRouter)
 app.use('/api/products', productRouter)
+
+// catch errors
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App is listening on port ${port}`))
